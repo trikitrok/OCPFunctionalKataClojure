a04b213d9d5745729a20767fd1c27b648e942ded
trikitrok <trikitrok@gmail.com>
2015-10-20 14:59:34 +0200

    Made local binding a private var in namespace.

--- src/ocp/core.clj
+++ src/ocp/core.clj
@@ -3,7 +3,14 @@
 (defn- multiple-of? [m n]
   (zero? (mod n m)))
 
-(defn- make-particular-way-of-saying [{:keys [pred say-as]}]
+(def ^:private particular-ways-of-saying-numbers
+  [{:pred (partial multiple-of? 3) :say-as "Fizz"}
+   {:pred (partial multiple-of? 5) :say-as "Buzz"}
+   {:pred (partial multiple-of? 7) :say-as "Bang"}
+   {:pred #(contains? #{11 13 17} %) :say-as "Klang"}])
+
+(defn- make-particular-way-of-saying
+  [{:keys [pred say-as]}]
   (fn [{:keys [acc n] :as so-far}]
     (if (pred n)
       (assoc so-far :acc (str acc say-as))
@@ -15,16 +22,11 @@
     acc))
 
 (defn- make-say-function []
-  (let [particular-ways-of-saying-numbers
-        [{:pred (partial multiple-of? 3) :say-as "Fizz"}
-         {:pred (partial multiple-of? 5) :say-as "Buzz"}
-         {:pred (partial multiple-of? 7) :say-as "Bang"}
-         {:pred #(contains? #{11 13 17} %) :say-as "Klang"}]]
-    (->> particular-ways-of-saying-numbers
-         reverse
-         (map make-particular-way-of-saying)
-         (cons just-say)
-         (apply comp))))
+  (->> particular-ways-of-saying-numbers
+       reverse
+       (map make-particular-way-of-saying)
+       (cons just-say)
+       (apply comp)))
 
 (def ^:private say-number (make-say-function))
 
